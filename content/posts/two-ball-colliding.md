311602dad25d265a3bddc17c1e6b1659ebea328b
wayne wang <cymatics@foxmail.com>
2019-10-23 20:11:42 +0800

    test video

---
+++ content/posts/two-ball-colliding.md
@@ -0,0 +1,18 @@
+---
+title: 弹性碰撞与圆周率的计算
+categories: 数学
+keywords: 'bubbles'
+thumbnail: "../thumbnails/c44.png"
+
+tags:
+  - bubbles
+date: 2019-10-23 10:51:34
+---
+
+====================  
+测试视频的嵌入
+
+<iframe height=500 width=100% src="//player.bilibili.com/player.html?aid=61421110&cid=106845214&page=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"> </iframe>
+
+====================  
+
--- gatsby-config.js
+++ gatsby-config.js
@@ -58,6 +58,16 @@ module.exports = {
               throwOnError:false,
               errorColor:"#cc0000",
             }
+          },
+          {
+            resolve: "gatsby-remark-embed-video",
+            options: {
+              width: 800,
+              ratio: 1.77, // Optional: Defaults to 16/9 = 1.77
+              height: 400, // Optional: Overrides optional.ratio
+              related: false, //Optional: Will remove related videos from the end of an embedded bilibili video.
+              noIframeBorder: true, //Optional: Disable insertion of <style> border: 0,
+             }
           }
         ],
       },
