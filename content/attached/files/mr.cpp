d12726650c621a61e164e5fd817c43d8c8388b00
Du, Zhao Hui <zhaohuidu2016@163.com>
2022-05-15 07:24:12 +0800

    searching for 6x6 matrix with same row/col product

--- content/attached/files/mr.cpp
+++ content/attached/files/mr.cpp
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <time.h>
-#include <set>
+#include <vector>
+#include <algorithm>
 
 #ifdef DEBUG
-#define TRACE(fmt,...) printf(fmt, __VA_ARGS__)
+#define TRACE(fmt,...) printf(fmt, __VA_ARGS__);fflush(stdout)
 #else
 #define TRACE(fmt, ...)
 #endif
@@ -21,10 +22,13 @@ void initlist()
     int i,j,v;
     for(i=0;i<TNUM;i++){
 	    v=i;
+	    printf("%02d:{",i);
 	    for(j=0;j<NUMP;j++){
 		    list[i][j]=v%order[j];
+		    printf(" %d",list[i][j]);
 		    v/=order[j];
 	    }
+	    printf(" }\n");
     }
 }
 
@@ -45,7 +49,7 @@ void search()
 	}
 }
 long tv;
-std::set<long> tvs;
+std::vector<long> tvs;
 void output()
 {
 	int i,j;
@@ -61,7 +65,7 @@ void output()
 		if(sum[j]!=(order[j]-1)*HROW)break;
 	}
 	if(j==NUMP){
-		tvs.insert(tv);
+		tvs.push_back(tv);
 	}
 }
 
@@ -86,11 +90,56 @@ long rowid[ROW];
 long columnid[ROW];
 long rowmask, colmask;
 long cmbcount;
+int firstbit(long x){
+	int i;
+	for(i=0;i<36;i++){
+		if(x&(1l<<i))return i;
+	}
+	return -1;
+}
 void outputrows()
 {
-	if(cmbcount%10==0){
-		printf("%ld\n",cmbcount);
-	}
+	char x[6]={0,1,2,3,4,5};
+	char y[6]={0,1,2,3,4,5};
+	int sum1[NUMP],sum2[NUMP];
+	do{
+		do{
+			int i,j;
+			long v1=0,v2=0;
+			int failed = 0;
+			for(i=0;i<6;i++){
+				if(v1&rowid[x[i]]&columnid[y[i]]){
+					failed=1;break;
+				}
+				v1|=rowid[x[i]]&columnid[y[i]];
+				if(v2&rowid[x[i]]&columnid[y[ROW-1-i]]){
+					failed=1;break;
+				}
+				v2|=rowid[x[i]]&columnid[y[ROW-1-i]];
+			}
+			for(i=0;i<tvs.size();i++){
+				if(v1==tvs[i])break;
+			}
+			if(i==tvs.size())continue;
+			for(i=0;i<tvs.size();i++){
+				if(v2==tvs[i])break;
+			}
+			if(i==tvs.size())continue;
+			printf("Sol:\n");
+			for(i=0;i<6;i++){
+				for(j=0;j<6;j++){
+					long h=rowid[x[i]]&columnid[y[j]];
+					printf("%02d,",firstbit(h));
+				}
+				printf("\n");
+			}
+			printf("\n");
+			fflush(stdout);
+			return;
+
+		}while(std::next_permutation(x,x+6));
+	}while(std::next_permutation(y,y+6));
+
 }
 
 int is_single_bit(long x)
@@ -99,60 +148,58 @@ int is_single_bit(long x)
 	return (x&(x-1))==0;
 }
 
-void searchrows(std::set<long>::iterator& rit, std::set<long>::iterator& cit)
+void searchrows(int rit, int cit)
 {
 	int i;
 	int rc=0;
-	std::set<long>::iterator hit,lit;
+	int hit,lit;
 	if(step==ROW){
 		cmbcount++;
 		outputrows();
 		return;
 	}
-	for(hit = rit; hit!= tvs.end();++hit){
+	for(hit = rit; hit!= tvs.size();++hit){
 		rc++;
-		if(rowmask&*hit)continue;
+		if(rowmask&tvs[hit])continue;
 		if(step==1)printf("\t");
 		if(step<=1){
 			int tdiff=time(NULL)-starttime;
-			printf("Search row %d for step %d(%d seconds)\n",rc,step, tdiff);
+			fprintf(stderr,"Search row %d for step %d(%d seconds)\n",rc,step, tdiff);
 		}
 		for(i=0;i<step;i++){
-			if(!is_single_bit(*hit&columnid[i]))
+			if(!is_single_bit(tvs[hit]&columnid[i]))
 				break;
 		}
 		if(i<step)continue;
-		if(step>=4){
+		if(step>=5){
 			for(i=0;i<step;i++){
 				TRACE("row %d v %09lx ", i, rowid[i]);
 			}
-			TRACE("row %d v %09lx\n",step, *hit);
+			TRACE("row %d v %09lx\n",step, tvs[hit]);
 		}
-		rowid[step]=*hit;
-		rowmask|=*hit;
-		for(lit = cit; lit!=tvs.end(); ++lit){
-		    if(colmask&*lit)continue;
+		rowid[step]=tvs[hit];
+		rowmask|=tvs[hit];
+		for(lit = cit; lit!=tvs.size(); ++lit){
+		    if(colmask&tvs[lit])continue;
 		    for(i=0;i<=step;i++){
-			    if(!is_single_bit(*lit & rowid[i]))
+			    if(!is_single_bit(tvs[lit] & rowid[i]))
 				    break;
 		    }
 		    if(i<=step)continue;
-		    if(step>=4){
+		    if(step>=5){
 			    for(i=0;i<step;i++){
 				    TRACE("col %d v %09lx",i,columnid[i]);
 			    }
-		    	TRACE("col %d v %09lx\n",step, *lit);
+		    	TRACE("col %d v %09lx\n",step, tvs[lit]);
 		    }
-		    columnid[step]=*lit;
-		    colmask|=*lit;
+		    columnid[step]=tvs[lit];
+		    colmask|=tvs[lit];
 		    step++;
-		    std::set<long>::iterator n1it=hit;
-		    std::set<long>::iterator n2it=lit;
-		    searchrows(++n1it,++n2it);
-		    colmask&=~*lit;
+		    searchrows(hit+1,lit+1);
+		    colmask&=~tvs[lit];
 		    step--;
 		}
-		rowmask &= ~*hit;
+		rowmask &= ~tvs[hit];
 	}
 }
 
@@ -161,9 +208,8 @@ int main()
 	starttime = time(NULL);
 	initlist();
 	searchrow(0);
-	printf("Total %ld rows found\n",tvs.size());
-	std::set<long>::iterator sit = tvs.begin();
-	searchrows(sit,sit);
+	printf("Total %ld rows found\n",tvs.size());fflush(stdout);
+	searchrows(0,0);
 	printf("%ld\n",cmbcount);
 	return 0;
 }
